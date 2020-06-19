c4f83eae20ce0e85e875caccc88329456d7670fe
apcesk <apcesk@163.com>
2020-06-19 20:50:15 +1200

    设置请求方法错误捕获

--- admin/src/http.js
+++ admin/src/http.js
@@ -1,9 +1,19 @@
 // 接口相关的方法封装的文件
 
 import axios from 'axios';
-
+import Vue from 'vue';
 const http = axios.create({
     baseURL: 'http://localhost:3000/admin/api'
 });
+
+http.interceptors.response.use(res =>{
+    return res;
+}, err =>{
+    console.log(err.response.data);
+    if (err.response.data.message){
+        Vue.prototype.$message({message:err.response.data.message, type: 'error'})
+    }
+    return Promise.reject(err);
+})
 
 export default http;
