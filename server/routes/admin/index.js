83899866019f41d900b6da8778d832cc1d19079a
apcesk <apcesk@163.com>
2020-06-19 20:54:41 +1200

    静态文件，其实都是一张图片

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -55,6 +55,38 @@ module.exports = app => {
         const file = await req.file;
         file.url = `http://localhost:3000/uploads/${file.filename}`;
         res.send(file);
+    });
+
+    // 登录接口
+    app.post('/admin/api/login', async (req,res)=>{
+        // res.send('ok')
+        // console.log(req)
+        const {username, password} = req.body;
+        // console.log(username, password);
+        // 1:找用户，根据用户名找用户
+        const AdminUser = require('../../models/AdminUser');
+        const user = await AdminUser.findOne({username}).select('+password')
+        console.log(user);
+        if (!user){
+            return res.status(422).send({
+                message:'用户不存在'
+            })
+        }
+        // 2:校验密码
+        const isValid = require('bcrypt').compareSync(password, user.password);
+        if (!isValid){
+            return res.status(422).send({
+                message: '密码错误'
+            })
+        }
+        // 3:返回token
+        const jwt = require('jsonwebtoken');
+        const token = jwt.sign({
+            id: user._id,
+            // _id: user._id,
+            // username: user.username,
+        }, app.get('secret'));
+        res.send({token});
     })
 
 } 
