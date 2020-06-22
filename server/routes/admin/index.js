e31fa1f93ee71a346007e706f3e1ea6d013ff38d
apcesk <apcesk@163.com>
2020-06-22 19:39:54 +1200

    需求文件更新以及后台接口修正

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -1,23 +1,33 @@
 // 后端路由
 module.exports = app => {
     const express = require('express');
-    const router = express.Router({
+    const jwt = require('jsonwebtoken');
+    const AdminUser = require('../../models/AdminUser');
+    const assert = require('http-assert');    const router = express.Router({
         mergeParams: true
     });
+    // 登录校验中间件
+    const authMiddleware = require('../../middleware/auth')
+
+    // 模型中间件
+    const resouceMiddleware = require('../../middleware/resource');
+    app.use('/admin/api/rest/:resource', authMiddleware(), resouceMiddleware(),router);
+
+
     // const req.Model = require('../../models/req.Model');
     // 创建分类的接口
-    router.post('/', async (req,res)=>{
+    router.post('/', authMiddleware(), async (req,res)=>{
         
         const model = await req.Model.create(req.body);
         res.send(model); 
     })
     // 更新数据的接口
-    router.put('/:id', async (req,res)=>{
+    router.put('/:id', authMiddleware(), async (req,res)=>{
         const model = await req.Model.findByIdAndUpdate(req.params.id, req.body);
         res.send(model); 
     })
     // 删除数据的接口
-    router.delete('/:id', async (req,res)=>{
+    router.delete('/:id', authMiddleware(), async (req,res)=>{
         await req.Model.findByIdAndDelete(req.params.id, req.body);
         res.send({
             success: true
@@ -25,7 +35,7 @@ module.exports = app => {
     })
     
     // 分类列表的接口
-    router.get('/', async (req,res)=>{
+    router.get('/', authMiddleware(),async (req,res)=>{
         const queryOptions = {};
         if (req.Model.modelName === 'Category'){
             queryOptions.populate = 'parent'
@@ -35,23 +45,18 @@ module.exports = app => {
         res.send(items); 
     })
     // 分类列表详情的接口(用于修改某条数据)
-    router.get('/:id', async (req,res)=>{
+    router.get('/:id', authMiddleware(), async (req,res)=>{
         // console.log(req.id);
         const model = await req.Model.findById(req.params.id)
         // console.log(items)
         res.send(model); 
     })
-    app.use('/admin/api/rest/:resource', async (req, res, next)=>{
-        const modelName = require('inflection').classify(req.params.resource);
-        req.Model = require(`../../models/${modelName}`);
-        next();
-    },router);
 
 
     const multer = require('multer');
     const upload = multer({dest: __dirname + '/../../uploads'})
     // 获取上传的文件接口
-    app.post('/admin/api/upload', upload.single('file'), async (req,res)=>{
+    app.post('/admin/api/upload',authMiddleware(), upload.single('file'), async (req,res)=>{
         const file = await req.file;
         file.url = `http://localhost:3000/uploads/${file.filename}`;
         res.send(file);
@@ -64,23 +69,14 @@ module.exports = app => {
         const {username, password} = req.body;
         // console.log(username, password);
         // 1:找用户，根据用户名找用户
-        const AdminUser = require('../../models/AdminUser');
+        
         const user = await AdminUser.findOne({username}).select('+password')
-        console.log(user);
-        if (!user){
-            return res.status(422).send({
-                message:'用户不存在'
-            })
-        }
-        // 2:校验密码
+        // console.log(user);
+        assert(user, 422, '用户不存在');
+        
         const isValid = require('bcrypt').compareSync(password, user.password);
-        if (!isValid){
-            return res.status(422).send({
-                message: '密码错误'
-            })
-        }
-        // 3:返回token
-        const jwt = require('jsonwebtoken');
+        assert(isValid, 422, '密码错误');
+        
         const token = jwt.sign({
             id: user._id,
             // _id: user._id,
@@ -89,4 +85,9 @@ module.exports = app => {
         res.send({token});
     })
 
+    // 错误处理函数
+    app.use(async (err, req, res, next)=>{
+        // console.log(err);
+        res.status(err.statusCode || 500).send({message: err.message})
+    })
 } 
