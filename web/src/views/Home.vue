af6f9920ec18ad514f24802075ff6476687951fa
apcesk <apcesk@163.com>
2020-07-09 12:50:29 +1200

    首页轮播图

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
     首页
+    <swiper>
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/1872789c248441e82ab767d31bb22845.jpeg" alt="">
+      </swiper-slide>
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/7e20896a31cd76fb78486c5240123ab9.jpeg" alt="">
+      </swiper-slide>
+      <swiper-slide>
+        <img class="w-100" src="../assets/images/9c5e6fa4d04fd4b3b2a9a0b2db86b3cd.jpeg" alt="">
+      </swiper-slide>
+
+    </swiper>
   </div>
 </template>
 
