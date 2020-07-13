b54026bce1e5cd8fc3760fe850bff7d6904fbf4c
apcesk <apcesk@163.com>
2020-07-13 19:37:33 +1200

    自定义card组件

---
+++ web/src/components/Card.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="card p-3 bg-white mt-3">
+        <div class="card-header d-flex pb-3">
+          <i class="iconfont" :class="`icon-${icon}`"></i>
+          <div class="fs-xl flex-1 px-2">{{title}}</div>
+          <i class="iconfont icon-shengluo"></i>
+        </div>
+        <div class="card-body pt-3">
+          <slot></slot>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props:{
+        title: {type:String, required: true},
+        icon: {type:String, required: true},
+    }
+}
+</script>
+<style lang="scss">
+   .card{
+       .card-header{
+           border-bottom: 1px solid #eee;
+       }
+       border-bottom: 1px solid #eee;
+   }
+</style>
